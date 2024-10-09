5d2d261f016957fd3347534adfd7e7958355ea4e
John Cubides <john.cubides87@gmail.com>
2024-10-09 15:51:47 -0500

    Initial commit: Spring Cloud Gateway setup

---
+++ src/test/java/Linktic/Test/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package Linktic.Test.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
